8f11c0f3df3d7e317f5dc31076eb440846f1b770
winters-ag <winters.alan.g@gmail.com>
2023-10-23 20:59:28 -0500

    fixed email error message

--- formik-app/src/App.js
+++ formik-app/src/App.js
@@ -18,7 +18,7 @@ function App() {
       if(!values.email){
         errors.email = 'Field required';
       } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        errors.email = 'Invalid email address'
+        errors.email = 'Username should be an email'
       } 
       
       if(!values.password) errors.password = 'Field required';
